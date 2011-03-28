b518207121be76d6bfabc18ec4af137b351b5a1e
Tomer Keshet <tomer.keshet@gmail.com>
2011-03-28 16:16:45 +0200

    Add LocationTagService
    Allows to get task tags that are marked with @.
    Changed MapActivity to an example of using it.

--- astrid/src/com/todoroo/astrid/activity/MapLocationActivity.java
+++ astrid/src/com/todoroo/astrid/activity/MapLocationActivity.java
@@ -2,17 +2,31 @@ package com.todoroo.astrid.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.aroundroidgroup.locationTags.LocationTagService;
 import com.timsu.astrid.R;
+import com.todoroo.astrid.data.Task;
 
 public class MapLocationActivity extends Activity {
     public static final String MAP_EXTRA_TASK = "task"; //$NON-NLS-1$
 
+    private Task mCurrentTask = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_main);
+        Bundle b = getIntent().getExtras();
+        mCurrentTask = (Task)b.getParcelable(MAP_EXTRA_TASK) ;
+
+        TextView tv = (TextView) findViewById(R.id.textview);
+        tv.setText(LocationTagService.getLocationTags(mCurrentTask.getId())[0]);
+
+
+
+
+
     }
 
 }
