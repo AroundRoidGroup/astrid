bc80f964e61c95d5c86849d9142c19c1458642ed
Tomer Keshet <tomer.keshet@gmail.com>
2011-04-03 12:30:20 +0300

    Alon Added GPS From Moti NOTWORK

--- astrid/src/com/todoroo/astrid/activity/myService.java
+++ astrid/src/com/todoroo/astrid/activity/myService.java
@@ -1,44 +1,46 @@
 package com.todoroo.astrid.activity;
+import java.util.HashSet;
+import java.util.Set;
+
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.aroundroidgroup.locationTags.LocationTagService;
+import com.aroundroidgroup.map.Misc;
 import com.todoroo.andlib.data.TodorooCursor;
 import com.todoroo.andlib.sql.Criterion;
 import com.todoroo.andlib.sql.Query;
-import com.todoroo.andlib.utility.DateUtilities;
 import com.todoroo.astrid.core.SortHelper;
 import com.todoroo.astrid.dao.TaskDao.TaskCriteria;
 import com.todoroo.astrid.data.Task;
+import com.todoroo.astrid.reminders.Notifications;
 import com.todoroo.astrid.service.TaskService;
 
 public class myService extends Service{
-    Integer sum = 0;
     boolean isThreadOn = false;
     public final String TAG = "myService";
+    Set<Long> alreadyNotified = new HashSet<Long>();
+    Notifications notificatons = new Notifications();
 
     @Override
     public void onCreate() {
         super.onCreate();
         Toast.makeText(this,"onCreate", Toast.LENGTH_LONG).show();
         Log.d(TAG," onCreate");
+        //Misc.gpsSetup(this);
         new nearReminder().start();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        /*if(!isThreadOn)
-  	   {
-  		   isThreadOn = true;
-  		   SumCalc sumCalc = new SumCalc();
-  		   sumCalc.start();
-   	   	   Toast.makeText(this,"onStartCommand. Run New Thread", Toast.LENGTH_LONG).show();
-    	}
-  	   else
-	   	   Toast.makeText(this,"onStartCommand. sum is:" + sum, Toast.LENGTH_LONG).show();
-         */
+        if(!isThreadOn)
+        {
+            isThreadOn = true;
+            new nearReminder().start();
+        }
         return START_STICKY;
     }
     @Override
@@ -58,37 +60,44 @@ public class myService extends Service{
 
         @Override
         public void run() {
+            while(true){
+                TaskService taskService = new TaskService();
 
-            //Task task = (Task) b.getParcelable(MAP_EXTRA_TASK);
-            //long itemId = task.getId();
 
-            //   Toast.makeText(myService.this,"onStartCommand. Run New Thread", Toast.LENGTH_LONG).show();
 
-            TaskService taskService = new TaskService();
+                TodorooCursor<Task> cursor = taskService.query(Query.select(Task.ID, Task.TITLE,
+                        Task.IMPORTANCE, Task.DUE_DATE).where(Criterion.and(TaskCriteria.isActive(),
+                                TaskCriteria.isVisible())).
+                                orderBy(SortHelper.defaultTaskOrder()).limit(30));
+                try {
 
-            int a =0;
-            while(DateUtilities.now()%120000>200)
-                a++;
-
-            TodorooCursor<Task> cursor = taskService.query(Query.select(Task.ID, Task.TITLE,
-                    Task.IMPORTANCE, Task.DUE_DATE).where(Criterion.and(TaskCriteria.isActive(),
-                            TaskCriteria.isVisible())).
-                            orderBy(SortHelper.defaultTaskOrder()).limit(30));
-            /*try {
-
-                Task task = new Task();
-                for (int i = 0; i < cursor.getCount(); i++) {
-                    cursor.moveToNext();
-                    task.readFromCursor(cursor);
-                    if (motifunc(task).length>0)
-                        new Notifications().showTaskNotification(task.getId(),
-                                ReminderService.TYPE_SNOOZE, "You are near");
+                    Task task = new Task();
+                    for (int i = 0; i < cursor.getCount(); i++) {
+                        cursor.moveToNext();
+                        task.readFromCursor(cursor);
+                        for (String str: LocationTagService.getLocationTags(task.getId()))
+                            notify(task.getId(),str);
                     }
-            } finally {
-                cursor.close();
-            }*/
+                } finally {
+                    cursor.close();
+                }
+            }
+        }
 
-            //taskService.query(new Query())
+        private void notify(long id, String str) {
+            if (Misc.getPlacesAroundMe(str, 100, 5).isEmpty()){
+                if (alreadyNotified.contains(id)){
+                    alreadyNotified.remove(id);
+                 //   Notifications.cancelNotifications(id);
+                }
+            }else{
+                if (!alreadyNotified.contains(id)){
+                    alreadyNotified.add(id);
+                  //  notificatons.showTaskNotification(id,
+                  //          ReminderService.TYPE_SNOOZE, "You are near");
+                }
+            }
+
         }
     }
 
