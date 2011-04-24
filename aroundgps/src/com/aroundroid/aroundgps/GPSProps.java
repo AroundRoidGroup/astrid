02f8dd10d5d77a83991f5ff5a6d6c1cc8555eaac
Tomer Keshet <tomer.keshet@gmail.com>
2011-04-25 02:20:56 +0300

    Added The AppEngine Project For People Location Handling

---
+++ aroundgps/src/com/aroundroid/aroundgps/GPSProps.java
@@ -0,0 +1,71 @@
+package com.aroundroid.aroundgps;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.users.User;
+
+@PersistenceCapable
+public class GPSProps {
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Key key;
+    
+    @Persistent
+    private String mail;
+
+    @Persistent
+    private User user;
+
+    @Persistent
+    private Double lon;
+
+    @Persistent
+    private Double lat;
+
+    public GPSProps(User user,String mail, double lon, double lat) {
+    	this.setMail(mail);
+        this.user = user;
+        this.lon = lon;
+        this.lat = lat;
+    }
+
+    public Key getKey() {
+        return key;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public double getLong() {
+        return lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setAuthor(User user) {
+        this.user = user;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+}
