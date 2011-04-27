2d086f0bacc376d9a9dca5775fe816ed790d16cf
Tomer Keshet <tomer.keshet@gmail.com>
2011-04-27 20:49:39 +0300

    Changed People Search To work with Database correctly

--- astrid/src/com/aroundroidgroup/astrid/googleAccounts/AroundRoidAppConstants.java
+++ astrid/src/com/aroundroidgroup/astrid/googleAccounts/AroundRoidAppConstants.java
@@ -1,7 +1,36 @@
 package com.aroundroidgroup.astrid.googleAccounts;
 
+import java.util.Collection;
+import java.util.Iterator;
+
+
 public class AroundRoidAppConstants {
     public static final String appMain = "https://aroundroid.appspot.com"; //$NON-NLS-1$
     public static final String gpsUrl = appMain + "/aroundgps"; //$NON-NLS-1$
     public static final String loginUrl = appMain+"/_ah/login?continue=http://localhost/&auth="; //$NON-NLS-1$
+    public static final String usersDelimiter = "::"; //$NON-NLS-1$
+
+    //TODO : find proper method
+    public static String join(Collection<String> s, String delimiter) {
+        StringBuffer buffer = new StringBuffer();
+        Iterator<String> iter = s.iterator();
+        while (iter.hasNext()) {
+            buffer.append(iter.next());
+            if (iter.hasNext()) {
+                buffer.append(delimiter);
+            }
+        }
+        return buffer.toString();
+    }
+
+    public static String join(String[] sArr, String delimiter) {
+        StringBuffer buffer = new StringBuffer();
+        for ( int i = 0; i < sArr.length ; i++){
+            buffer.append(sArr[i]);
+            if (i < sArr.length-1){
+                buffer.append(delimiter);
+            }
+        }
+        return buffer.toString();
+    }
 }
