88ed9441c400a19706ed6b72bca65d041174a0b7
Shiran Chityat <shiran5chityat@gmail.com>
2011-05-23 17:00:54 +0300

    temporarly removed the activation of GPSService

--- astrid/src/com/todoroo/astrid/activity/TaskListActivity.java
+++ astrid/src/com/todoroo/astrid/activity/TaskListActivity.java
@@ -58,7 +58,6 @@ import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
 
 import com.aroundroidgroup.astrid.googleAccounts.AccountList;
-import com.aroundroidgroup.astrid.gpsServices.GPSService;
 import com.timsu.astrid.R;
 import com.todoroo.andlib.data.Property;
 import com.todoroo.andlib.data.TodorooCursor;
@@ -236,8 +235,8 @@ public class TaskListActivity extends ListActivity implements OnScrollListener,
         */
 
 
-        Intent serviceIntent = new Intent(this,GPSService.class);
-        Toast.makeText(this, "GPSService is on:"+(startService(serviceIntent)!=null)+"", Toast.LENGTH_LONG).show(); //$NON-NLS-1$ //$NON-NLS-2$
+        //Intent serviceIntent = new Intent(this,GPSService.class);
+      //  Toast.makeText(this, "GPSService is on:"+(startService(serviceIntent)!=null)+"", Toast.LENGTH_LONG).show(); //$NON-NLS-1$ //$NON-NLS-2$
 
 
       //Intent serviceIntent = new Intent(this,myService.class);
