89c9a04b9d04fe40061d5102a2d4f58c67385fd3
Tomer Keshet <tomer.keshet@gmail.com>
2011-05-26 13:37:40 +0300

    Google gray button, creds list empty message

--- astrid/src/com/aroundroidgroup/astrid/googleAccounts/AccountList.java
+++ astrid/src/com/aroundroidgroup/astrid/googleAccounts/AccountList.java
@@ -13,27 +13,30 @@ import com.aroundroidgroup.astrid.gpsServices.GPSService;
 import com.timsu.astrid.R;
 
 public class AccountList extends ListActivity {
-	protected AccountManager accountManager;
-	protected Intent intent;
+    protected AccountManager accountManager;
+    protected Intent intent;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.creds_list);
         accountManager = AccountManager.get(getApplicationContext());
         Account[] accounts = accountManager.getAccountsByType("com.google");
-        this.setListAdapter(new ArrayAdapter<Account>(this, R.layout.creds_list_item, accounts));
+        if (accounts.length>0){
+            this.setListAdapter(new ArrayAdapter<Account>(this, R.layout.creds_list_item, accounts));
+        }
     }
 
-	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-		Account account = (Account)getListView().getItemAtPosition(position);
-		GPSService.account = account;
-		GPSService.connectCount = 1;
-		finish();
-		/*Intent intent = new Intent(this, AppInfo.class);
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Account account = (Account)getListView().getItemAtPosition(position);
+        GPSService.account = account;
+        GPSService.connectCount = 1;
+        finish();
+        /*Intent intent = new Intent(this, AppInfo.class);
 		intent.putExtra("account", account);
 		startActivity(intent);
-		*/
-	}
+         */
+    }
 }
