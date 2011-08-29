fd625a4880c3541307e677a8728532dede62c7dd
Tomer Keshet <tomer.keshet@gmail.com>
2011-08-30 02:35:55 +0300

    fixed moti UI bug

--- astrid/src/com/todoroo/astrid/activity/SpecificMapLocation.java
+++ astrid/src/com/todoroo/astrid/activity/SpecificMapLocation.java
@@ -826,7 +826,7 @@ public class SpecificMapLocation extends MapActivity{
     @Override
     protected void onPause() {
         super.onPause();
-        //        mHan.removeCallbacks(mUpdateTimeTask);
+        mHan.removeCallbacks(mUpdateTimeTask);
         mLocationDB.close();
         saveAndQuit();
     }
